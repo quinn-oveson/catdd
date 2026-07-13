3e480ebc6dfb37f9a7da83e37c03bb4adb5d828e
quinn oveson <qoveson@gmail.com>
2026-07-13 17:00:36 -0600

    set config to belkin set up with new learning rate that matches their results better

--- mnist_dd/config.py
+++ mnist_dd/config.py
@@ -3,27 +3,27 @@ import torch.nn as nn
 D = 784
 # K = number of classes (number of possible digits)
 K = 10
-# N_TRAIN = number of training points (from Belkin)
+# N_TRAIN = number of training points (Belkin = 4000)
 N_TRAIN = 4000
 # random seed for choosing training points
 SEED = 0
 # learning rate (not specified by Belkin)
-LR = 0.05
-# momentum (from Belkin)
+LR = 0.1
+# momentum (Belkin = 0.95)
 MOMENTUM = 0.95
-# maximum number of epochs before stopping (from Belkin)
+# maximum number of epochs before stopping (Belkin = 6000)
 MAX_EPOCHS = 6000
-# how many epochs between learning rate decays (from Belkin)
+# how many epochs between learning rate decays (Belkin = 500)
 DECAY_INTERVAL = 500
-# decay factor for learning rate (from Belkin)
+# decay factor for learning rate (Belkin = 0.9 for 10% decay)
 GAMMA = 0.9
 # these are guessed from the graph
-H_VALS = [4.,    5.,    8.,   13.,   18.,   23.,   25.,   28.,   30.,
-         33.,   35.,   38.,   40.,   43.,   45.,   48.,   50.,   54.,
-         58.,   63.,   75.,  101.,  201.,  314., 1006.]
+# H_VALS = [4.,    5.,    8.,   13.,   18.,   23.,   25.,   28.,   30., 33.,   35.,   38.,   40.,   43.,   45.,   48.,   50.,   54., 58.,   63.,   75.,  101.,  201.,  314., 1006.]
+H_VALS = [5, 6, 9, 13, 15, 25, 30, 34, 38, 41, 44, 47, 48, 49, 50, 51, 57, 63, 88, 107, 252, 314, 1006]
+# H_VALS = [40, 43, 45, 48, 50, 54]
 # variance for new weights in models that are resuing weights from past (not specified by Belkin)
 VAR = 0.01
-# number of trials to average for each H value (from Belkin)
+# number of trials to average for each H value (Belkin = 5)
 N_TRIALS = 5
 # random seed list so the train set will change for each of the 5 iterations
 SEEDS = range(N_TRIALS)
@@ -39,11 +39,11 @@ ALWAYS_STOP = False
 # reuse weights in underparameterized models (Belkin = True)
 REUSE_WEIGHTS_UNDERPARAM = True
 # reuse weights in overparameterized models (Belkin = False)
-REUSE_WEIGHTS_OVERPARAM = True
+REUSE_WEIGHTS_OVERPARAM = False
 # Label for file name
-CONFIG_LABEL = "mse_always_reuse_always_decay_always_stop"
+CONFIG_LABEL = "higher_lr_belkin_setup_5_trials"
 # Plot title
-PLOT_TITLE = "MSE Loss, Always Reuse Weights, Conditional LR Decay and Stopping"
+PLOT_TITLE = "LR=0.1, Belkin Setup"
 # True = zoomed-in plot (skip first 3 H values, tick marks starting at 14)
 # False = full plot (all H values, tick marks starting at 3)
 ZOOMED_PLOT = True
